4d6e2225d70f6fba6334235bf38b1a2dbbb4df26
Charles <119467519+cdemzy@users.noreply.github.com>
2025-08-27 11:56:29 -0300

    enabled cors for services

--- depemp-backend/Program.cs
+++ depemp-backend/Program.cs
@@ -1,8 +1,23 @@
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Enable CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
 
 var app = builder.Build();
 
