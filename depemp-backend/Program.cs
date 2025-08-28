b88ce38deccc5b5a02f303ca32b99d2effdc9819
Charles <119467519+cdemzy@users.noreply.github.com>
2025-08-28 17:54:26 -0300

    enabled newtonjson in services

--- depemp-backend/Program.cs
+++ depemp-backend/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,13 @@ builder.Services.AddCors(options =>
                   .AllowAnyHeader();
         });
 });
+
+// JSON Serializer
+builder.Services.AddControllersWithViews()
+    .AddNewtonsoftJson(options => 
+        options.SerializerSettings
+               .ReferenceLoopHandling=Newtonsoft.Json.ReferenceLoopHandling.Ignore).AddNewtonsoftJson(options => 
+                    options.SerializerSettings.ContractResolver = new DefaultContractResolver());
 
 var app = builder.Build();
 
