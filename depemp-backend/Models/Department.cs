1aafc620b6e27ad7257f1569f2d5756e113b408f
Charles <119467519+cdemzy@users.noreply.github.com>
2025-08-28 18:24:29 -0300

    created department and employee models

---
+++ depemp-backend/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace depemp_backend.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+
+        public string DepartmentName { get; set; }
+    }
+}
