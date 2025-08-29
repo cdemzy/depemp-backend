3e20f0916bf63d23f4d8f0a991d9fe2e5f6b6b2b
Charles <119467519+cdemzy@users.noreply.github.com>
2025-08-29 11:28:16 -0300

    implemented HTTP methods for Department

--- depemp-backend/Controllers/DepartmentController.cs
+++ depemp-backend/Controllers/DepartmentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using depemp_backend.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
 using System.Data.SqlClient;
@@ -15,22 +16,22 @@ namespace depemp_backend.Controllers
             _configuration = configuration;
         }
 
+        // Get Method
         [HttpGet]
         public JsonResult Get()
         {
-            string query = @" 
-                            SELECT DepartmentId, DepartmentName
+            string query = @"
+                            SELECT DepartmentId, DepartmentName 
                             FROM dbo.Department
                             ";
 
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("DepEmpConnection");
             SqlDataReader myReader;
-
-            using(SqlConnection myCon = new SqlConnection(sqlDataSource))
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
             {
                 myCon.Open();
-                using(SqlCommand myCommand = new SqlCommand(query, myCon))
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
                 {
                     myReader = myCommand.ExecuteReader();
                     table.Load(myReader);
@@ -41,5 +42,92 @@ namespace depemp_backend.Controllers
 
             return new JsonResult(table);
         }
+
+        [HttpPost]
+        public JsonResult Post(Department dep)
+        {
+            string query = @"
+                           insert into dbo.Department
+                           values (@DepartmentName)
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DepEmpConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@DepartmentName", dep.DepartmentName);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+
+        [HttpPut]
+        public JsonResult Put(Department dep)
+        {
+            string query = @"
+                           update dbo.Department
+                           set DepartmentName= @DepartmentName
+                            where DepartmentId=@DepartmentId
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DepEmpConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@DepartmentId", dep.DepartmentId);
+                    myCommand.Parameters.AddWithValue("@DepartmentName", dep.DepartmentName);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                           delete from dbo.Department
+                            where DepartmentId=@DepartmentId
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DepEmpConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@DepartmentId", id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+
+
     }
 }
